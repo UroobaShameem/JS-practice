fa697b230cc7967f8a3c9bbcfb376fe468f9d12c
UroobaShameem <uroobashameem12@gmail.com>
2023-03-13 23:46:13 +0500

    commit

---
+++ ConditionalsLoops/task2.js
@@ -0,0 +1,17 @@
+// previous code using switch statement
+
+for(i=0;i<10;i++){
+    switch(i){
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver medal");
+            break;
+        case 3:
+            console.log("Bronze medal");
+            break;
+        default:
+            console.log(i);
+    }
+}
